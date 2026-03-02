0527c34258cb1bf7ce23698aa87d0048311056a1
Rodrigo <rodri.m.dls@gmail.com>
2026-03-02 03:08:36 +0200

    Integrates SELECTION part into a loop structure

--- main.py
+++ main.py
@@ -61,48 +61,49 @@ for t in t_list:
     print(t)
 
 # start loop
-
-# Check losing condition, find the cheapest option vs player money
-t_fees = []
-for t in t_list:
-    t['fee'] = t['prize_money'] * TOURNAMENT_FEE_RATE
-    t_fees.append(t['fee'])
-
-if player['money'] < min(t_fees) + TRAVEL_FEE:
-    print("Oh no! You don't have enough money to continue playing.")  # the game stops and quit
-    print("Game Over.")
-    sys.exit()
-
-f.show_tournaments(t_list) # Display options
-
-# Assign the decision to 'selected_tournament'
 while True:
-    user_input = input("Enter your option: ")
-    if user_input.isnumeric():
-        user_input_num = int(user_input)
+    # Check losing condition, find the cheapest option vs player money
+    t_fees = []
+    for t in t_list:
+        t['fee'] = t['prize_money'] * TOURNAMENT_FEE_RATE
+        t_fees.append(t['fee'])
 
-        if 1 <= user_input_num <= len(t_list):
-            selected_tournament = t_list[user_input_num - 1]
-            break
+    if player['money'] < min(t_fees) + TRAVEL_FEE:
+        print("Oh no! You don't have enough money to continue playing.")  # the game stops and quit
+        print("Game Over.")
+        sys.exit()
+
+    f.show_tournaments(t_list) # Display options
+
+    # Assign the decision to 'selected_tournament'
+    while True:
+        user_input = input("Enter your option: ")
+        if user_input.isnumeric():
+            user_input_num = int(user_input)
+
+            if 1 <= user_input_num <= len(t_list):
+                selected_tournament = t_list[user_input_num - 1]
+                break
+            else:
+                print(f"Please enter a valid number of tournament, between 1 and {len(t_list)}")
         else:
-            print(f"Please enter a valid number of tournament, between 1 and {len(t_list)}")
+            print("Select the tournament with a valid number.")
+
+    print(selected_tournament)
+
+    if player['money'] < (selected_tournament['fee']) + TRAVEL_FEE:
+        print("You don't have enough money to play this tournament. Please choose another from the list.") # we need to send the user back to the tournament selection screen
+        continue
+
     else:
-        print("Select the tournament with a valid number.")
-
-print(selected_tournament)
-
-if player['money'] < (selected_tournament['fee']) + TRAVEL_FEE:
-    print("You don't have enough money to play this tournament. Please choose another from the list.") # we need to send the user back to the tournament selection screen
-
-else:
-    print(f"{selected_tournament['name']} is played in {selected_tournament['city']}, {selected_tournament['country']}. The tournament gives {selected_tournament['points']} points and ${selected_tournament['prize_money']} to the champion. The entrance cost of the tournament is {selected_tournament['fee']} and the travel expenses are {TRAVEL_FEE}.")
+        print(f"{selected_tournament['name']} is played in {selected_tournament['city']}, {selected_tournament['country']}. The tournament gives {selected_tournament['points']} points and ${selected_tournament['prize_money']} to the champion. The entrance cost of the tournament is {selected_tournament['fee']} and the travel expenses are {TRAVEL_FEE}.")
 
 
-if input("Do you want to play the tournament? (Y/N)").upper() == "N":
-    print("Ok. Please choose another from the list.") # we need to send the user back to the tournament selection screen
-else:
-    print(f"Great!, flying to {selected_tournament['country']} ...")
-#     break
+    if input("Do you want to play the tournament? (Y/N) ").upper() == "N":
+        print("Ok. Please choose another from the list.") # we need to send the user back to the tournament selection screen
+    else:
+        print(f"Great!, flying to {selected_tournament['country']} ...")
+        break
 
 current_tournament = selected_tournament
 # The tournament is today! Press enter to start.
